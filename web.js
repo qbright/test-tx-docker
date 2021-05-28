c61db54bc05ae91790da6db645514ac6c0919e72
qbright <502268502@qq.com>
2021-05-28 22:45:36 +0800

    init project

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var http = require('http');
   
 http.createServer(function(request,response){
         response.writeHead(200,{'Content-Type':'text/html'});
-        response.end('Hello World\n');
+        response.end('Hello World\n fffffffffffffffffff');
 }).listen(8080);
 
 console.log('Server running at http://127.0.0.1:8080');
